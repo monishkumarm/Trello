ad6d9057781fe1343e2ff78011fa0d4a729bc7d9
Monish <monishkumarm@outlook.com>
2022-04-29 10:44:18 +0530

    FIX: CORS Issue

--- src/main/java/com/iiitb/trello/config/SecurityConfig.java
+++ src/main/java/com/iiitb/trello/config/SecurityConfig.java
@@ -29,9 +29,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         http.csrf()
-                .disable()
-                .cors()
                 .disable()
                 .authorizeRequests()
                 .antMatchers("/token").permitAll()
